3aa21cefcf8c08150ce1123c93fcfcb30dd98b0a
Hyunji Kim <s2208@e-mirim.hs.kr>
2023-12-30 15:35:48 +0900

    성공

---
+++ fetchData.php
@@ -0,0 +1,15 @@
+<?php
+include('db_conn.php');
+
+$stmt = $conn->prepare("SELECT * FROM project");
+$stmt->execute();
+$result = $stmt->get_result();
+
+$rows = array();
+while ($row = $result->fetch_assoc()) {
+    $rows[] = $row;
+}
+
+// 데이터를 JSON으로 반환
+echo json_encode($rows);
+?>
